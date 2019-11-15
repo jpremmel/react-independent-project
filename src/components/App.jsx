b5cc6ec0fa1e9e7b1fb42d0b943b81e96e1e83f5
Jacqueline Remmel <jpremmel@gmail.com>
2019-11-15 08:45:22 -0800

    Add initial template and App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+    return (
+      <div>
+        {/* <Switch>
+          <Route exact path='/' component={} />
+        </Switch> */}
+      </div>
+    );
+  }
+  
+  export default App;
