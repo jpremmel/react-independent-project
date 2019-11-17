f1666d7643ed2f98390ced9d688eaea8bc476338
Jacqueline Remmel <jpremmel@gmail.com>
2019-11-16 20:35:24 -0800

    Update styling of login form

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -22,16 +22,37 @@ function LoginPage() {
   var loginBtnStyle = {
     backgroundColor: '#2c2321',
   };
+  var placeholderText = {
+    fontFamily: 'Libre Franklin, sans-serif',
+  };
   return (
     <div style={loginParentStyle}>
+      <style jsx>{`
+        .input-field input[type=text]:focus {
+          border-bottom: 1px solid #2c2321;
+          box-shadow: 0 1px 0 0 #2c2321;
+        }
+        .input-field input[type=password]:focus {
+          border-bottom: 1px solid #2c2321;
+          box-shadow: 0 1px 0 0 #2c2321;
+        }
+      `}</style>
       <h3 style={loginText}>Employee log in</h3>
       <form>
-        <input
-          type='text'
-          placeholder='Username or Employee ID'/>
-        <input
-          type='password'
-          placeholder='Password'/>
+        <div className='input-field'>
+          <input
+            type='text'
+            placeholder='Username or Employee ID'
+            style={placeholderText}
+          />
+        </div>
+        <div className='input-field'>
+          <input
+            type='password'
+            placeholder='Password'
+            style={placeholderText}
+          />
+        </div>
         <div style={loginBtnParent}>
           <Link to='/new' style={loginBtnStyle} className="waves-effect waves-light btn-large"><i className="material-icons right">chevron_right</i>Log in</Link>
         </div>
